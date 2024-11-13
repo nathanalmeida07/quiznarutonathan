0498afeb26393d7f137b873488de5aa42fca5e11
nathanalmeida07 <00001099986448sp@al.educacao.sp.gov.br>
2024-11-13 11:11:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'anime',
+    'quantos caminhos de pain o jyraia conseguio derrotar ?',
+    '3'
+)
+
+criaCartao(
+    'anime',
+    'qual foi a primeira invocaçao do naruto ?',
+    'gamabuta'
+)
+
+criaCartao(
+    'anime',
+    'contra quais ninjas era permitido fugir do campo de batalhas se fosse encontrado?',
+    'minato e shisui'
+)
+
+criaCartao(
+    'anime',
+    'quantos espadachins da nevoa o pai do gai conseguio matar ?',
+    '4'
+)
